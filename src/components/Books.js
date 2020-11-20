14d894fffed4fe89ea711a38b23393475919c292
Andrea Selmi <andreaselmi90@gmail.com>
2020-11-20 18:56:38 +0100

    settato index risultati

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,21 +1,31 @@
 import React, { useContext, useEffect } from "react";
 import Book from "./Book";
 import { BookContext } from "../context/BookContext";
-import { CardGroup, Container, Row } from "react-bootstrap";
+import { CardGroup, Container, Row, Col } from "react-bootstrap";
 import _ from "lodash";
 import noimage from "../img/no-image.jpg";
 
 const Books = () => {
-  const { books, fetchData } = useContext(BookContext);
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line
-  }, []);
+  const { books, fetchData, startIndex, getIndex } = useContext(BookContext);
 
   return (
     <>
       <Container>
+        <Container>
+          <Row>
+            <Col>
+              Risultati: {startIndex} - {startIndex + 10}
+            </Col>
+            <Col className="text-right">
+              <button onClick={getIndex} value="+">
+                +
+              </button>
+              <button onClick={getIndex} value="-">
+                -
+              </button>
+            </Col>
+          </Row>
+        </Container>
         <CardGroup>
           <Row>
             {books.map((book) => (
@@ -38,3 +48,18 @@ const Books = () => {
 };
 
 export default Books;
+
+{
+  /* <i
+
+  className="fas fa-arrow-left"
+  value="2"
+></i>
+</label>
+
+<i
+onClick={getIndex}
+className="fas fa-arrow-right"
+value="1"
+></i> */
+}
--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 export const BookContext = createContext();
@@ -6,10 +6,11 @@ export const BookContext = createContext();
 const BooksContextProvider = (props) => {
   const [books, setBooks] = useState([]);
   const [search, setSearch] = useState("Madame Bovary");
+  const [startIndex, setStartIndex] = useState(0);
 
   const fetchData = async () => {
     const result = await axios(
-      `https://www.googleapis.com/books/v1/volumes?q=${search}`
+      `https://www.googleapis.com/books/v1/volumes?q=${search}&startIndex=${startIndex}`
     );
     console.log(result);
     const data = await result.data.items;
@@ -20,12 +21,28 @@ const BooksContextProvider = (props) => {
     setSearch(e.target.value);
   };
 
+  const getIndex = (e) => {
+    const sign = e.target.value;
+    if (sign === "-") {
+      if (startIndex === 0) {
+        return startIndex;
+      } else setStartIndex(startIndex - 10);
+    } else setStartIndex(startIndex + 10);
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line
+  }, [startIndex]);
+
   return (
     <BookContext.Provider
       value={{
         books,
         fetchData,
         getSearch,
+        startIndex,
+        getIndex,
       }}
     >
       {props.children}
